39d27666bf206031880edef384b8eaab9884d52f
mb0470 <47418020+mb0470@users.noreply.github.com>
2019-02-07 12:09:30 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,14 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+    var dividendo;
+    var divisor;
+    var resto;
+    dividendo = parseInt(dividendo);
+    divisor = parseInt(divisor);
+    dividendo = document.getElementById("numeroDividendo").value;
+    divisor = document.getElementById("numeroDivisor").value;
+    resto = parseInt(dividendo) / parseInt(divisor);
+    resto = parseInt(resto);
+    alert(resto);
 }
